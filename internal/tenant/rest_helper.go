bef774983ff59f3e24b3e515c3a93683ac2d799f
msukkari <michael.sukkarieh@mail.mcgill.ca>
2025-01-14 15:44:37 -0800

    remove unneeeded logs

--- internal/tenant/rest_helper.go
+++ internal/tenant/rest_helper.go
@@ -3,7 +3,6 @@ package tenant
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/sourcegraph/zoekt/internal/tenant/internal/tenanttype"
@@ -12,9 +11,7 @@ import (
 )
 
 func InjectTenantFromHeader(ctx context.Context, header http.Header) (context.Context, error) {
-	log.Printf("header: %v", header)
 	tenantID := header.Get("X-TENANT-ID") // TODO: we don't use headerKeyTenantID here so we don't need to change it and potentially break future grpc changes
-	log.Printf("tenantID: %s", tenantID)
 	if tenantID != "" {
 		tenant, err := tenanttype.Unmarshal(tenantID)
 		if err != nil {
